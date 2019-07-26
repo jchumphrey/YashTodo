6954034035ca1233d4dc32db8758b8440335cd40
Jake <jacobchumphrey@gmail.com>
2019-07-26 10:06:13 -0500

    First Commit

---
+++ FinalTodo/CustomScripts/BuildTable.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        url: '/Todoes/BuildTodoTable',
+        success: function (result) {
+            $('#tableDiv').html(result);
+        }
+    });
+
+});
---
+++ FinalTodo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalTodo.Startup))]
+namespace FinalTodo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
